a2f7b9614191a02e3d5548784b2c1fdd9c06e565
Ever Merino <emjr@udel.edu>
2026-03-08 12:51:26 -0400

    finished part 1 of try phaser

--- src/game/scenes/level1.ts
+++ src/game/scenes/level1.ts
@@ -1,34 +1,49 @@
-import { EventBus } from "../event-bus";
+//import { EventBus } from "../event-bus";
 import { Scene } from "phaser";
 
-import PhaserLogo from "../objects/phaser-logo";
-import FpsText from "../objects/fps-text";
+//import PhaserLogo from "../objects/phaser-logo";
+//import FpsText from "../objects/fps-text";
 
 export class Level1 extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    phaserLogo: PhaserLogo;
-    fpsText: FpsText;
+    private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    //phaserLogo: PhaserLogo;
+    //fpsText: FpsText;
 
     constructor() {
         super("Level1");
     }
 
     create() {
-        this.camera = this.cameras.main;
+        this.add.image(400, 300, "sky");
+        //this.add.image(400, 300, "star");
+        this.platforms = this.physics.add.staticGroup();
+        const ground = this.platforms.create(
+            400,
+            568,
+            "platform",
+        ) as Phaser.Physics.Arcade.Sprite;
+        ground.setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, "platform");
+        this.platforms.create(50, 250, "platform");
+        this.platforms.create(750, 220, "platform");
+
+        /*this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
         this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
 
         this.phaserLogo = new PhaserLogo(this, this.cameras.main.width / 2, 0);
-        this.fpsText = new FpsText(this);
+        //this.fpsText = new FpsText(this);
 
-        EventBus.emit("current-scene-ready", this);
+        EventBus.emit("current-scene-ready", this);*/
     }
 
     update() {
-        this.fpsText.update();
+        //    this.fpsText.update();
     }
 
     changeScene() {
--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -26,16 +26,26 @@ export class Preloader extends Scene {
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath("assets");
 
-        this.load.image("logo", "logo.png");
+        this.load.image("bomb", "bomb.png");
+        this.load.spritesheet("dude", "dude.png", {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
+        this.load.image("platform", "platform.png");
         this.load.image("star", "star.png");
+        this.load.image("sky", "sky.png");
+        this.load.image("bg", "bg.png");
+        this.load.image("star2", "star2.png");
+        this.load.image("logo", "logo.png");
         this.load.image("phaser-logo", "phaser-logo.png");
     }
 
     create() {
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
-
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start("MainMenu");
+        //this.add.image(400, 300, "sky");
+        //this.add.image(400, 300, "star");
+        this.scene.start("Level1");
     }
 }
