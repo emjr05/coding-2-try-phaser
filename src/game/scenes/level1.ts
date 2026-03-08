199ea7a210dc32819f8fb1c7ad92572131853fcb
Ever Merino <emjr@udel.edu>
2026-03-08 13:38:31 -0400

    part 2 finished

--- src/game/scenes/level1.ts
+++ src/game/scenes/level1.ts
@@ -8,6 +8,8 @@ export class Level1 extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
     //phaserLogo: PhaserLogo;
     //fpsText: FpsText;
 
@@ -30,6 +32,38 @@ export class Level1 extends Scene {
         this.platforms.create(50, 250, "platform");
         this.platforms.create(750, 220, "platform");
 
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
         /*this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
@@ -44,6 +78,19 @@ export class Level1 extends Scene {
 
     update() {
         //    this.fpsText.update();
+        if (this.cursors?.left.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        } else if (this.cursors?.right.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        } else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
+        if (this.cursors?.up.isDown && this.player?.body?.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 
     changeScene() {
