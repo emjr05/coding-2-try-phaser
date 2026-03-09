e23ea80bfd7628acb0182699b02c848daf9f4904
Ever Merino <emjr@udel.edu>
2026-03-09 04:33:10 -0400

    finished part 3

--- src/game/scenes/level1.ts
+++ src/game/scenes/level1.ts
@@ -1,5 +1,8 @@
 //import { EventBus } from "../event-bus";
 import { Scene } from "phaser";
+export type Collidable =
+    | Phaser.Types.Physics.Arcade.GameObjectWithBody
+    | Phaser.Tilemaps.Tile;
 
 //import PhaserLogo from "../objects/phaser-logo";
 //import FpsText from "../objects/fps-text";
@@ -10,6 +13,12 @@ export class Level1 extends Scene {
     private platforms?: Phaser.Physics.Arcade.StaticGroup;
     private player?: Phaser.Physics.Arcade.Sprite;
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+    private stars?: Phaser.Physics.Arcade.Group;
+    private bombs?: Phaser.Physics.Arcade.Group;
+
+    private score = 0;
+    private scoreText?: Phaser.GameObjects.Text;
+    private gameOver = false;
     //phaserLogo: PhaserLogo;
     //fpsText: FpsText;
 
@@ -64,6 +73,43 @@ export class Level1 extends Scene {
         this.physics.add.collider(this.player, this.platforms);
 
         this.cursors = this.input.keyboard?.createCursorKeys();
+
+        this.stars = this.physics.add.group({
+            key: "star",
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 },
+        });
+
+        this.stars.children.iterate((c) => {
+            const child = c as Phaser.Physics.Arcade.Image;
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            return true;
+        });
+
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.overlap(
+            this.player,
+            this.stars,
+            this.handleCollectStar,
+            undefined,
+            this,
+        );
+
+        this.scoreText = this.add.text(16, 16, "score: 0", {
+            fontSize: "32px",
+            color: "#000",
+        });
+
+        this.bombs = this.physics.add.group();
+        this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(
+            this.player,
+            this.bombs,
+            this.handleHitBomb,
+            undefined,
+            this,
+        );
+
         /*this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
@@ -76,6 +122,40 @@ export class Level1 extends Scene {
         EventBus.emit("current-scene-ready", this);*/
     }
 
+    private handleCollectStar(player: Collidable, s: Collidable) {
+        const star = s as Phaser.Physics.Arcade.Image;
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText?.setText(`Score: ${this.score}`);
+        if (this.stars?.countActive(true) === 0) {
+            this.stars.children.iterate((c) => {
+                const child = c as Phaser.Physics.Arcade.Image;
+                child.enableBody(true, child.x, 0, true, true);
+                return true;
+            });
+            if (this.player) {
+                const x =
+                    this.player.x < 400 ?
+                        Phaser.Math.Between(400, 800)
+                    :   Phaser.Math.Between(0, 400);
+                const bomb = this.bombs?.create(
+                    x,
+                    16,
+                    "bomb",
+                ) as Phaser.Physics.Arcade.Image;
+                bomb.setBounce(1);
+                bomb.setCollideWorldBounds(true);
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            }
+        }
+    }
+
+    private handleHitBomb(player: Collidable, b: Collidable) {
+        this.physics.pause();
+        this.player?.setTint(0xff0000);
+        this.player?.anims.play("turn");
+        this.gameOver = true;
+    }
     update() {
         //    this.fpsText.update();
         if (this.cursors?.left.isDown) {
